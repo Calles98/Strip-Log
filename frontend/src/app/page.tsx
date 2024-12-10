321739d05676597b9641e8059d0048529dabb566
Jose Calles <josecalles@Joses-MacBook-Pro.local>
2024-12-10 12:57:15 -0700

    Completed Lithology plot

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -30,10 +30,11 @@ export default function Home() {
     });
 
     try {
-      const response = await fetch("http://localhost:5000/upload", {
+      const response = await fetch("http://127.0.0.1:5000/upload", {
         method: "POST",
         body: formData,
       });
+      
 
       if (!response.ok) {
         throw new Error("Failed to upload files");
